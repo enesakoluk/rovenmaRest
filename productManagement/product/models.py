6f9fd5b339b97c64360e788068fb496bc766aa13
enesakoluk <66967159+enesakoluk@users.noreply.github.com>
2021-11-08 18:48:17 +0300

    Update models.py

--- productManagement/product/models.py
+++ productManagement/product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 class Products(models.Model):
     id=models.AutoField(primary_key=True)
     name = models.TextField()
